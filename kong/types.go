70c6f53d58951bdce02805111baba4133e73b414
Tyler Rivera <tyler_rivera@comcast.com>
2020-06-17 08:55:22 -0400

    chore: remove unused type definition

--- kong/types.go
+++ kong/types.go
@@ -221,16 +221,6 @@ type Workspace struct {
 	Meta      map[string]interface{} `json:"meta,omitempty" yaml:"meta,omitempty"`
 }
 
-// Workspace Entity represents a WorkspaceEntity in Kong
-type WorkspaceEntity struct {
-	EntityID         *string `json:"entity_id,omitempty" yaml:"entity_id,omitempty"`
-	EntityType       *string `json:"entity_type,omitempty" yaml:"entity_type,omitempty"`
-	UniqueFieldName  *string `json:"unique_field_name,omitempty" yaml:"unique_field_name,omitempty"`
-	UniqueFieldValue *string `json:"unique_field_value,omitempty" yaml:"unique_field_value,omitempty"`
-	WorkspaceID      *string `json:"workspace_id,omitempty" yaml:"workspace_id,omitempty"`
-	WorkspaceName    *string `json:"workspace_name,omitempty" yaml:"workspace_name,omitempty"`
-}
-
 // Admin represents an Admin in Kong.
 type Admin struct {
 	CreatedAt        *int    `json:"created_at,omitempty" yaml:"created_at,omitempty"`
